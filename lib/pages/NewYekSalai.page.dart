e6b8971cfa77fbce4dc28ae0424fdc26aa7ba2f1
jessi khuman <jessikhuman@jessis-MacBook-Air.local>
2024-02-05 10:37:48 +0530

    j

--- lib/pages/NewYekSalai.page.dart
+++ lib/pages/NewYekSalai.page.dart
@@ -1,27 +1,36 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:yeksalai/constant/constant.dart';
 
 @RoutePage()
-class NewYekSalaiPage extends StatelessWidget {
+class NewYekSalaiPage extends StatefulWidget {
   const NewYekSalaiPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    double scHeight = MediaQuery.of(context).size.height;
-    double scWidth = MediaQuery.of(context).size.width;
+  State<NewYekSalaiPage> createState() => _NewYekSalaiPageState();
+}
 
+class _NewYekSalaiPageState extends State<NewYekSalaiPage> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(viewportFraction: 0.99);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: scHeight,
-        width: scHeight,
+      body: Container(margin: const EdgeInsets.all(8),
         decoration: const BoxDecoration(
-          // image: DecorationImage(
-          //     scale: 1,
-          //     fit: BoxFit.cover,
-          //     // alignment: Alignment(0, -.9),
-          //     image: AssetImage(
-          //       "assets/images/BG/YellowBG.jpeg",
-          //     )),
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomLeft,
@@ -33,20 +42,71 @@ class NewYekSalaiPage extends StatelessWidget {
             ],
           ),
         ),
-        child: Stack(
-          fit: StackFit.expand,
-          alignment: Alignment.center,
-          children: [
-            Image.asset(
-              "assets/images/BG/redBG.jpg",
-              fit: BoxFit.fill,
-            ),
-            Image.asset("assets/images/splash/yellow.png", fit: BoxFit.contain),
-            Image.asset(
-              "assets/images/char/char2.png",
-              fit: BoxFit.contain,
-            ),
-          ],
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return PageView.builder(
+              controller: _pageController,
+              itemCount: dataMap.length,
+              itemBuilder: (context, index) {
+                return AnimatedBuilder(
+                  animation: _pageController,
+                  builder: (context, child) {
+                    double value = 1.0;
+                    if (_pageController.position.haveDimensions && constraints.maxWidth != 0) {
+                      value = (_pageController.page! - index).abs();
+                      value = 1 - (value * 0.5).clamp(0.0, 1.0);
+                    }
+                    return Center(
+                      child: SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            
+                            Transform.translate(
+                              offset: Offset(value * constraints.maxWidth * 0.0, 0.2),
+                              child: SizedBox(
+                                height: MediaQuery.of(context).size.height * 0.4,
+                                width: MediaQuery.of(context).size.width * 0.4,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image.asset(
+                                    dataMap[index]["BG"],
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: Offset(-value * constraints.maxWidth * 0.11, 0.2),
+                              child: Positioned.fill(
+                                child: Image.asset(dataMap[index]["splash"], fit: BoxFit.contain),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: Offset(0.0, value * constraints.maxHeight * 0.2),
+                              child: Positioned.fill(
+                                child: Image.asset(
+                                  dataMap[index]["char"],
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            ),
+                             Text(dataMap[index]["Yek"], style: const TextStyle(fontSize: 68),).animate(onPlay: (controller) => controller.repeat())
+        .shimmer(duration: 4444.ms, color: const Color(0xFF80DDFF))
+        .animate() // this wraps the previous Animate in another Animate
+        // .fadeIn(duration: 1200.ms, curve: Curves.easeOutQuad)
+        // .slide(),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
         ),
       ),
     );
