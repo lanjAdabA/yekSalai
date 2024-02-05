9c059778bf045fb62ab8561a38beaf1d3d3082f4
lanjAdabA <thingbaijamjessikhuman@gmail.com>
2024-02-05 15:19:16 +0530

    parent widget error fixed

--- lib/pages/NewYekSalai.page.dart
+++ lib/pages/NewYekSalai.page.dart
@@ -16,7 +16,7 @@ class _NewYekSalaiPageState extends State<NewYekSalaiPage> {
 
   @override
   void initState() {
-    _pageController = PageController(viewportFraction: 1.08);
+    _pageController = PageController(viewportFraction: 1);
     super.initState();
   }
 
@@ -29,100 +29,98 @@ class _NewYekSalaiPageState extends State<NewYekSalaiPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomLeft,
-            stops: [0.1, 0.5, 0.7],
-            colors: [
-              Color.fromARGB(255, 201, 176, 245),
-              Color.fromARGB(255, 225, 211, 248),
-              Color.fromARGB(255, 234, 229, 246),
-            ],
-          ),
-        ),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return PageView.builder(
-              controller: _pageController,
-              itemCount: dataMap.length,
-              itemBuilder: (context, index) {
-                return AnimatedBuilder(
-                  animation: _pageController,
-                  builder: (context, child) {
-                    double value = 1.0;
-                    if (_pageController.position.haveDimensions &&
-                        constraints.maxWidth != 0) {
-                      value = (_pageController.page! - index).abs();
-                      value = 1 - (value * 0.5).clamp(0.0, 1.0);
-                    }
-                    return Center(
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height * .96,
-                        width: MediaQuery.of(context).size.width * .96,
-                        child: Stack(
-                          fit: StackFit.expand,
-                          children: [
-                            Transform.translate(
-                              // Issue may be here, verify constraints.maxWidth usage
-                              offset: Offset(
-                                  value * constraints.maxWidth * 0.0, 0.2),
-                              child: SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.4,
-                                width: MediaQuery.of(context).size.width * 0.4,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(26),
-                                  child: Image.asset(
-                                    dataMap[index]["BG"],
-                                    fit: BoxFit.fill,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomLeft,
+                  stops: [0.1, 0.5, 0.7],
+                  colors: [
+                    Color.fromARGB(255, 201, 176, 245),
+                    Color.fromARGB(255, 225, 211, 248),
+                    Color.fromARGB(255, 234, 229, 246),
+                  ],
+                ),
+              ),
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: dataMap.length - 1,
+                itemBuilder: (context, index) {
+                  return AnimatedBuilder(
+                    animation: _pageController,
+                    builder: (context, child) {
+                      double value = 1.0;
+                      if (_pageController.position.haveDimensions &&
+                          constraints.maxWidth != 0) {
+                        value = (_pageController.page! - index).abs();
+                        value = 1 - (value * 0.5).clamp(0.0, 1.0);
+                      }
+                      return Center(
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height * .96,
+                          width: MediaQuery.of(context).size.width * .96,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Transform.translate(
+                                // Issue may be here, verify constraints.maxWidth usage
+                                offset: Offset(
+                                    value * constraints.maxWidth * 0.0, 0.2),
+                                child: SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.4,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(26),
+                                    child: Image.asset(
+                                      dataMap[index]["BG"],
+                                      fit: BoxFit.fill,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Transform.translate(
-                              // Issue may be here, verify constraints.maxWidth usage
-                              offset: Offset(
-                                  -value * constraints.maxWidth * 0.11, 0.2),
-                              child: Positioned.fill(
+                              Transform.translate(
+                                // Issue may be here, verify constraints.maxWidth usage
+                                offset: Offset(
+                                    -value * constraints.maxWidth * 0.11, 0.2),
                                 child: Image.asset(dataMap[index]["splash"],
                                     fit: BoxFit.contain),
-                              ),
-                            ),
-                            Transform.translate(
-                              // Issue may be here, verify constraints.maxHeight usage
-                              offset: Offset(
-                                  0.0, value * constraints.maxHeight * 0.2),
-                              child: Positioned.fill(
+                              ).animate().effect(
+                                  duration: const Duration(microseconds: 23)),
+                              Transform.translate(
+                                // Issue may be here, verify constraints.maxHeight usage
+                                offset: Offset(
+                                    0.0, value * constraints.maxHeight * .091),
                                 child: Image.asset(
                                   dataMap[index]["char"],
                                   fit: BoxFit.contain,
                                 ),
                               ),
-                            ),
-                            Text(
-                              dataMap[index]["Yek"],
-                              style: const TextStyle(fontSize: 68),
-                            )
-                                .animate(
-                                    onPlay: (controller) => controller.repeat())
-                                .shimmer(
-                                    duration: 4444.ms,
-                                    color: const Color(0xFF80DDFF))
-                                .animate() // this wraps the previous Animate in another Animate
-                            // .fadeIn(duration: 1200.ms, curve: Curves.easeOutQuad)
-                            // .slide(),
-                          ],
+                              Text(
+                                dataMap[index]["Yek"],
+                                style: const TextStyle(fontSize: 68),
+                              )
+                                  .animate(
+                                      onPlay: (controller) =>
+                                          controller.repeat())
+                                  .shimmer(
+                                      duration: 4444.ms,
+                                      color: const Color(0xFF80DDFF))
+                                  .animate() // this wraps the previous Animate in another Animate
+                              // .fadeIn(duration: 1200.ms, curve: Curves.easeOutQuad)
+                              // .slide(),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                );
-              },
-            );
-          },
-        ),
+                      );
+                    },
+                  );
+                },
+              ));
+        },
       ),
     );
   }
