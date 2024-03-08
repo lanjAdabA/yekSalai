05403f5e83ae132bfe1862df041a83793e396dec
lanjAdabA <thingbaijamjessikhuman@gmail.com>
2024-03-08 16:02:58 +0530

    push

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -6,18 +6,19 @@ List<Map<String, dynamic>> dataMap = [
   {
     "Yek": "Mangang",
     "color": Colors.red,
+    "Yelhen": "Ningthoulelbi",
     "BG": "assets/images/BG/redBG.jpg",
     "char": "assets/images/char/char1.png",
     "splash": "assets/images/splash/red.png",
     "YekInfo": [
       {
-        "name": "Machu",
-        "itemname": "Red",
+        "name": "Machu/Color",
+        "itemname": "Angangba/Red",
         "asset": "assets/images/color/oilRed.png"
       },
       {
         "name": "Lei/ Flower",
-        "itemname": "Red Lotus",
+        "itemname": "Thambal Angangba/Red Lotus",
         "asset": "assets/images/redLotus.png",
       },
       {
@@ -26,33 +27,38 @@ List<Map<String, dynamic>> dataMap = [
         "asset": "assets/images/thamchet.png"
       },
       {
-        "name": "Nga",
+        "name": "Nga/Fish",
         "itemname": "Sareng Angangba",
         "asset": "assets/images/sarengAngangba.jpg"
       },
       {
-        "name": "Thang",
+        "name": "Thang/Sword",
         "itemname": "Chak Thang",
         "asset": "assets/images/thangMangang.jpg"
       },
       {
-        "name": "Shing",
+        "name": "Shing/Firewood",
         "itemname": "Sayee",
         "asset": "assets/images/noData.jpg"
       },
-      {"name": "Ta", "itemname": "NA", "asset": "assets/images/noData.jpg"},
+      {
+        "name": "Ta/Spear",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
     ]
   },
   {
     "Yek": "Luwang",
     "color": Colors.white,
+    "Yelhen": "Mairen Angouba",
     "BG": "assets/images/BG/whiteBG.jpg",
     "char": "assets/images/char/char2.png",
     "splash": "assets/images/splash/white.png",
     "YekInfo": [
       {
-        "name": "Machu",
-        "itemname": "White",
+        "name": "Machu/Color",
+        "itemname": "Angouba/White",
         "asset": "assets/images/color/oilWhite.png"
       },
       {
@@ -62,37 +68,42 @@ List<Map<String, dynamic>> dataMap = [
       },
       {
         "name": "Hei/ Fruit",
-        "itemname": "Pineapple",
+        "itemname": "Kihom/Pineapple",
         "asset": "assets/images/pineapple.png"
       },
       {
-        "name": "Nga",
+        "name": "Nga/Fish",
         "itemname": "Sareng Angouba",
         "asset": "assets/images/sarengAngouba.png"
       },
       {
-        "name": "Thang",
+        "name": "Thang/Sword",
         "itemname": "Tondum Thang",
         "asset": "assets/images/thangLuwang.jpg"
       },
       {
-        "name": "Shing",
+        "name": "Shing/Firewood",
         "itemname": "Heikru",
         "asset": "assets/images/noData.jpg"
       },
-      {"name": "Ta", "itemname": "NA", "asset": "assets/images/noData.jpg"},
+      {
+        "name": "Ta/Spear",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
     ]
   },
   {
     "Yek": "Khuman",
     "color": Colors.black,
+    "Yelhen": "Tera",
     "BG": "assets/images/BG/blackBG.jpg",
     "char": "assets/images/char/char3.png",
     "splash": "assets/images/splash/black.png",
     "YekInfo": [
       {
-        "name": "Machu",
-        "itemname": "Black",
+        "name": "Machu/Color",
+        "itemname": "Amuba/Black",
         "asset": "assets/images/color/oilBlack.png"
       },
       {
@@ -106,38 +117,43 @@ List<Map<String, dynamic>> dataMap = [
         "asset": "assets/images/heibong.png"
       },
       {
-        "name": "Nga",
+        "name": "Nga/Fish",
         "itemname": "Sareng Amuba",
         "asset": "assets/images/sarengAmuba.png"
       },
       {
-        "name": "Thang",
+        "name": "Thang/Sword",
         "itemname": "Tonkak Thang",
         "asset": "assets/images/thangKhuman.jpg"
       },
       {
-        "name": "Shing",
+        "name": "Shing/Firewood",
         "itemname": "Chakhong",
         "asset": "assets/images/noData.jpg"
       },
-      {"name": "Ta", "itemname": "NA", "asset": "assets/images/noData.jpg"},
+      {
+        "name": "Ta/Spear",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
     ]
   },
   {
     "Yek": "Angom",
     "color": Colors.yellow,
+    "Yelhen": "Angom Yensil Akuppa",
     "BG": "assets/images/BG/yellowBG.jpg",
     "char": "assets/images/char/char4.png",
     "splash": "assets/images/splash/yellow.png",
     "YekInfo": [
       {
-        "name": "Machu",
-        "itemname": "Yellow",
+        "name": "Machu/Color",
+        "itemname": "Napu Machu/Yellow",
         "asset": "assets/images/color/oilYellow.png"
       },
       {
         "name": "Lei/ Flower",
-        "itemname": "Tharo Angouba",
+        "itemname": "Tharo Angouba/White water lily",
         "asset": "assets/images/tharoAngouba.png"
       },
       {
@@ -146,30 +162,39 @@ List<Map<String, dynamic>> dataMap = [
         "asset": "assets/images/heibong.png"
       },
       {
-        "name": "Nga",
+        "name": "Nga/Fish",
         "itemname": "Sareng Angouba",
         "asset": "assets/images/sarengAngouba.png"
       },
       {
-        "name": "Thang",
+        "name": "Thang/Sword",
         "itemname": "Pukak Thang",
         "asset": "assets/images/thangAngom.jpg"
       },
-      {"name": "Shing", "itemname": "NA", "asset": "assets/images/noData.jpg"},
-      {"name": "Ta", "itemname": "NA", "asset": "assets/images/noData.jpg"},
+      {
+        "name": "Shing/Firewood",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
+      {
+        "name": "Ta/Spear",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
     ]
   },
   {
     "Yek": "Moirang",
     "color": Colors.brown,
+    "Yelhen": "Moirang Khanambi",
     "BG": "assets/images/BG/brownBG.jpg",
     "char": "assets/images/char/char5.png",
     "splash": "assets/images/splash/brown.png",
     "YekInfo": [
       {
-        "name": "Machu",
-        "itemname": "Brown",
-        "asset": "assets/images/color/oilBrown.png"
+        "name": "Machu/Color",
+        "itemname": "Mangra machu/Brown",
+        "asset": "assets/images/color/oilbrown.png"
       },
       {
         "name": "Lei/ Flower",
@@ -182,27 +207,40 @@ List<Map<String, dynamic>> dataMap = [
         "asset": "assets/images/heitup.png"
       },
       {
-        "name": "Nga",
+        "name": "Nga/Fish",
         "itemname": "Sareng Napu",
         "asset": "assets/images/sarengNapu.png"
       },
       {
-        "name": "Thang",
+        "name": "Thang/Sword",
         "itemname": "Yentok Saba Thang",
         "asset": "assets/images/thangMoirang.jpg"
       },
-      {"name": "Shing", "itemname": "NA", "asset": "assets/images/noData.jpg"},
-      {"name": "Ta", "itemname": "NA", "asset": "assets/images/noData.jpg"},
+      {
+        "name": "Shing/Firewood",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
+      {
+        "name": "Ta/Spear",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
     ]
   },
   {
     "Yek": "Khaba Nganba",
     "color": Colors.teal,
+    "Yelhen": "Leipung Khanga",
     "BG": "assets/images/BG/blueBG.jpg",
     "char": "assets/images/char/char6.png",
-    "splash": "assets/images/splash/color/oilTeal.png",
+    "splash": "assets/images/splash/blue.png",
     "YekInfo": [
-      {"name": "Machu", "itemname": "Blue", "asset": "assets/images/teal.png"},
+      {
+        "name": "Machu/Color",
+        "itemname": "Blue",
+        "asset": "assets/images/color/oilTeal.png"
+      },
       {
         "name": "Lei/ Flower",
         "itemname": "Leishang",
@@ -214,29 +252,38 @@ List<Map<String, dynamic>> dataMap = [
         "asset": "assets/images/heiba.png"
       },
       {
-        "name": "Nga",
+        "name": "Nga/Fish",
         "itemname": "Sareng Ngangna Langba",
         "asset": "assets/images/sarengAngangba.jpg"
       },
       {
-        "name": "Thang",
+        "name": "Thang/Sword",
         "itemname": "Chinaiba Thang",
         "asset": "assets/images/thangKhanganba.jpg"
       },
-      {"name": "Shing", "itemname": "NA", "asset": "assets/images/noData.jpg"},
-      {"name": "Ta", "itemname": "NA", "asset": "assets/images/noData.jpg"},
+      {
+        "name": "Shing/Firewood",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
+      {
+        "name": "Ta/Spear",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
     ]
   },
   {
     "Yek": "Sarang Leishangthem",
     "color": Colors.green,
+    "Yelhen": "Sendang",
     "BG": "assets/images/BG/greenBG.jpg",
     "char": "assets/images/char/char7.png",
     "splash": "assets/images/splash/green.png",
     "YekInfo": [
       {
-        "name": "Machu",
-        "itemname": "Green",
+        "name": "Machu/Color",
+        "itemname": "Ashangba/Green",
         "asset": "assets/images/color/oilGreen.png"
       },
       {
@@ -250,17 +297,25 @@ List<Map<String, dynamic>> dataMap = [
         "asset": "assets/images/manamNaiba.png"
       },
       {
-        "name": "Nga",
+        "name": "Nga/Fish",
         "itemname": "Sareng Sangna Langba",
         "asset": "assets/images/sarengArangba.png"
       },
       {
-        "name": "Thang",
+        "name": "Thang/Sword",
         "itemname": "Khunthong Thang",
         "asset": "assets/images/thangSarangLeishangthem.jpg"
       },
-      {"name": "Shing", "itemname": "NA", "asset": "assets/images/noData.jpg"},
-      {"name": "Ta", "itemname": "NA", "asset": "assets/images/noData.jpg"},
+      {
+        "name": "Shing/Firewood",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
+      {
+        "name": "Ta/Spear",
+        "itemname": "NA",
+        "asset": "assets/images/noData.jpg"
+      },
     ]
   },
 ];
